f5c3a29866eb361cb76be64d2020f998bea56488
SlaxXxX <Janis.Schneider97@web.de>
2019-11-06 13:23:38 +0100

    added a working stereo camera test

---
+++ Project/NaoVR/Assets/Test/Webcam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Webcam : MonoBehaviour
+{
+
+    public string CameraName = "USB2.0 PC CAMERA";
+
+    void Start()
+    {
+        WebCamTexture texture = new WebCamTexture(CameraName);
+        Debug.Log("Available: " + string.Join(", ", WebCamTexture.devices.ToList().ConvertAll(device => device.name)));
+        Debug.Log("Chosen: " + texture.deviceName); // USB2.0 PC CAMERA // USB2.0 PC CAMERA 1
+
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = texture;
+        texture.Play();
+        Debug.Log("Playing " + texture.deviceName + " on " + gameObject.name);
+    }
+}
